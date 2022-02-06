cf637b9ddae9bffb9d2ed3b23b8c5712b8dc7516
Trending Tech <96788552+trendingtechbiz@users.noreply.github.com>
2022-02-06 14:18:00 +0100

    Update config.js

--- config.js
+++ config.js
@@ -1,2 +1,2 @@
 // MODIFICA ESTA URL POR LA DEL SERVICIO WEB DESPUES DE HABERLO DESPLEGADO
-const AS_URL_BASE = "https://script.google.com/macros/s/AKfycbyA03y1sBtIIRMcWGefO69Uq-TXj5YRMdehyj8p3ONkIax5zgl-93v53yg2jcLUcYlydg/exec";
+const AS_URL_BASE = "https://script.google.com/macros/s/AquiVaTuURL/exec";
