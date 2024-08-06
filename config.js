778d324af392533ae99ec0e97087d1f0658b2ab8
templarciber <templarciberpruebas@gmail.com>
2024-08-06 14:53:48 -0500

    Update config.js

--- config.js
+++ config.js
@@ -1,2 +1,2 @@
 // MODIFICA ESTA URL POR LA DEL SERVICIO WEB DESPUES DE HABERLO DESPLEGADO
-const AS_URL_BASE = "https://script.google.com/macros/s/AquiVaTuURL/exec";
+const AS_URL_BASE = "https://script.google.com/macros/s/AKfycby6KCGHVMQp6hSbR7uSl6eB_4cPSyD99pmYIBOx2RysWm3_Pf_aNIBFRK2MyB2rzXInQg/exec";
